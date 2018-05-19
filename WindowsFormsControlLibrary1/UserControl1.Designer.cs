3ac2df9e89e0a2f18b0ab0c90c05f9b6e4ac75b3
Денис Хрущев <squil@mail.ru>
2018-05-20 02:04:55 +0900

    Тест

--- WindowsFormsControlLibrary1/UserControl1.Designer.cs
+++ WindowsFormsControlLibrary1/UserControl1.Designer.cs
@@ -36,7 +36,7 @@
             // 
             this.button1.BackColor = System.Drawing.SystemColors.Control;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button1.Location = new System.Drawing.Point(78, 65);
+            this.button1.Location = new System.Drawing.Point(164, 74);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
